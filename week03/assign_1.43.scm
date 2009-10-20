7096895562c508273128f1403300baaa6a4ccb14
Tim Harper <timcharper@gmail.com>
2009-10-20 15:09:28 -0600

    1.43

---
+++ week03/assign_1.43.scm
@@ -0,0 +1,22 @@
+#lang scheme/base
+; create repeated
+
+(require (planet schematics/schemeunit:3))
+
+(define (compose f g)
+  (lambda (x) (f (g x)))
+  )
+(define (square x) (* x x))
+
+((compose square square) 2)
+
+(define (repeated f n)
+  (if (zero? n)
+      (lambda (x) x)
+      (compose f (repeated f (- n 1))))
+  )
+
+(check-eq?
+ ((repeated square 2) 5)
+ (square (square 5))
+ )
