d4e856fa5e0fe293a1ddad5cc3f95aa394571350
Tim Harper <timcharper@gmail.com>
2009-10-22 00:51:15 -0600

    refactored to use lambda

--- week03/assign_1.29.scm
+++ week03/assign_1.29.scm
@@ -1,70 +1,55 @@
 #lang scheme/base
-; Implement a procedure that takes f, a, b, and n, and returns the value of the integral.  Simpson's rule
-; http://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson.27s_rule
-
-; Simpsons rule:
-; a, b = range
-; n = fidelity operator - more intense computation, more accurate
-; h = (b - a) / n   <- in other words, the incrementer
-; k = 0..(b-a)
-; y(k) = f(a + kh)
+;; Implement a procedure that takes f, a, b, and n, and returns the value of the integral.  Simpson's rule
+;; http://en.wikipedia.org/wiki/Simpson%27s_rule#Composite_Simpson.27s_rule
+;; Simpsons rule:
+;; a, b = range
+;; n = fidelity operator - more intense computation, more accurate
+;; h = (b - a) / n   <- in other words, the incrementer
+;; k = 0..(b-a)
+;; y(k) = f(a + kh)
 
 (define (cube x) (* x x x))
-(define (sum term a next b) 
-  (if (> a b) 
-      0 
-      (+ (term a) 
-         (sum term (next a) next b)))) 
+(define (incrementor step) (lambda (k) (+ k step)))
+(define (sum function a b inc-function)
+  (if (> a b)
+      0
+      (+ (function a)
+         (sum function (inc-function a) b inc-function))))
 
-(define (integral f a b dx) 
-  (define (add-dx x) (+ x dx)) 
-  (* (sum f (+ a (/ dx 2.0)) add-dx b) 
+(define (integral f a b dx)
+  (* (sum f (+ a (/ dx 2.0)) b (incrementor dx))
      dx))
-
 
 (define (simpsons-integral-coefficient k)
   (if (even? k) 2 4)
   )
-(define (simpsons-integral-loop f a b h k n total)
-  (if (= n 0)
-      (* (/ h 3) total)
-      (simpsons-integral-loop
-       f
-       a
-       b
-       h
-       (+ k 1)
-       (- n 1)
-       (+ total (* (simpsons-integral-coefficient k) (f (+ a (* k h)))))
-       )
-      )
-  )
 
 (define (simpsons-integral f a b n)
-  (simpsons-integral-loop
-   f
-   a
-   b
-   (/ (- b a) n)
-   1
-   (- n 1)
-   (+ (f a) (f b))
+  (define h (/ (- b a) n))
+  (define (simpsons-inside-function f a h)
+    (lambda (k)
+      (*
+       (simpsons-integral-coefficient k)
+       (f (+ a (* k h))))))
+  (*
+   (/ h 3)
+   (+ (f a) (f b) (sum (simpsons-inside-function f a h) 1 (- n 1) (incrementor 1)))
    )
   )
 
+;; i f(x)_a_b = x^3 dx
+;;  (1/4 b^4) - (1/4 a^4)
+;;  (1/4 * 81) - (1/4 * 0)
+;;  (81 / 4)
+
 (integral cube 0 3 0.01)
-(integral cube 0 3 0.1)
-(integral cube 0 3 0.25)
 (simpsons-integral cube 0 3 100)
+(integral cube 0 3 0.1)
 (simpsons-integral cube 0 3 10)
+(integral cube 0 3 0.25)
 (simpsons-integral cube 0 3 4)
 
-; i f(x)_a_b = x^3 dx
-;  (1/4 b^4) - (1/4 a^4)
-;  (1/4 * 81) - (1/4 * 0)
-;  (81 / 4)
-
+(display "----- linear ----\n")
 (define (linear x) (+ 0 x))
 (integral linear 0 10 0.01)
 (simpsons-integral linear 0 10 4)
-
